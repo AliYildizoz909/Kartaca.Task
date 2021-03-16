f1eaf9061ad0a3f3a3c3d3d176640ff325723322
Ali <aliyildizoz909@gmail.com>
2021-03-16 14:11:46 +0300

    Add project files.

---
+++ Kartaca.Task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Kartaca.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public string SolveFileName(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new Exception("File name is null or empty.");
+            }
+            return Encoding.UTF8.GetString(Convert.FromBase64String(fileName));
+        }
+    }
+}
